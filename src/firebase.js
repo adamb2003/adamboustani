61258eeca5e0ff0016c2cf9b3512860a91a17df9
adamb2003 <adamboustani@gmail.com>
2025-02-24 19:44:39 +0000

    Initial Commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// My web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
